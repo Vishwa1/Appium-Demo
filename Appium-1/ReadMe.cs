30ba2fc29935117e66f98f2ecb7d8a8592de6532
Vishwajeet <vishwajeet_vibhute@yahoo.com>
2017-03-15 11:58:23 +0000

    Mobile automation testing
    A basic project on mobile automation testing

---
+++ Appium-1/ReadMe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appium_1
+{
+    class ReadMe
+    {
+        //How to find UDID?
+        
+        // Go to location where your adb is installed. Usually, it is at the location below.
+        //C:\Users\vvibhute\AppData\Local\Android\sdk\platform-tools
+        //& run the command --> adb devices -l
+
+        // Make sure you set the capabilities right
+
+        // Run the emulator first
+
+        //Run the Appium Server next (Do not forget to enter the capabilitiy details as shown in the attached image 
+
+        //Run the test. It should go to bing.com and search for Microsoft.
+    }
+}
---
+++ Appium-1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Remote;
+using System.Configuration;
+
+namespace Appium
+{
+    [TestClass]
+
+    public class UnitTest1
+    {
+        //create instance for Appium webdriver
+        AppiumDriver<AndroidElement> _driver;
+        
+        //protected string AllocateUI_URL { get; private set; } //Allocate 
+
+        [TestMethod]
+        public void TestBrowser()
+        {
+            DesiredCapabilities cap = new DesiredCapabilities();
+            cap.SetCapability("deviceName", "donatello");
+            cap.SetCapability("platformVersion", "6.0.0");
+            cap.SetCapability("udid", "169.254.190.187:5555");
+            cap.SetCapability("fullReset", "True");
+            cap.SetCapability(MobileCapabilityType.App, "Browser");
+            cap.SetCapability("platformName", "Android");
+            _driver = new AndroidDriver<AndroidElement>(new Uri("http://127.0.0.1:4723/wd/hub"), cap);
+
+            #region Ignore me
+            /*Allocate
+                string defaultUrl = string.Format("http://{0}/ZarionAllocate", Environment.MachineName);
+                var url = ConfigurationManager.AppSettings["AllocateURL"] ?? defaultUrl;
+                AllocateUI_URL = url;
+                _driver.Navigate().GoToUrl(url);
+                End Allocate*/ 
+            #endregion
+
+            _driver.Navigate().GoToUrl("http://www.bing.com");
+            _driver.FindElementByName("q").SendKeys("Microsoft");
+            _driver.FindElementByName("q").SendKeys(Keys.Enter);
+            
+        }
+        [TestCleanup]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+
+}
